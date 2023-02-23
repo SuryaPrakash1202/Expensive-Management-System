3bc32b08fe539fc5d180ea40a6d27c1cb95b5128
Surya Prakash <115409375+SuryaPrakash1202@users.noreply.github.com>
2023-02-24 01:42:37 +0530

    Add files via upload

---
+++ transectionRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  addTransection,
+  getAllTransection,
+} = require("../controllers/transectionCtrl");
+
+//router object
+const router = express.Router();
+
+//routes
+//add transection POST method
+router.post("/add-transection", addTransection);
+
+//get transection
+router.get("/get-transection", getAllTransection);
+
+module.exports = router;
